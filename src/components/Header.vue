bfa6339a46238bd673ce856694d4d4f89877321e
Shikai <seuwsk@gmail.com>
2021-11-08 00:13:53 -0500

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="m-content">
+    <h3>Welcome to VueBlog</h3>
+    <div class="block">
+      <el-avatar :size="50" :src="user.avatar"></el-avatar>
+      <div>{{ user.username }}</div>
+    </div>
+
+    <div class="maction">
+      <span><el-link href="/blogs">Blogs</el-link></span>
+      <el-divider direction="vertical"></el-divider>
+      <span><el-link type="success" href="/blog/add">publish a blog</el-link></span>
+
+      <el-divider direction="vertical"></el-divider>
+      <span v-show="!hasLogin"><el-link type="primary" href="/login">login</el-link></span>
+
+      <span v-show="hasLogin"><el-link type="danger" @click="logout">logout</el-link></span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    data() {
+      return {
+        user: {
+          username: 'Please login',
+          avatar: 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+        },
+        hasLogin: false
+      }
+    },
+    methods: {
+      logout() {
+        const _this = this
+        _this.$axios.get("/logout", {
+          headers: {
+            "Authorization": localStorage.getItem("token")
+          }
+        }).then(res => {
+          _this.$store.commit("REMOVE_INFO")
+          _this.$router.push("/login")
+
+        })
+      }
+    },
+    created() {
+      if(this.$store.getters.getUser.username) {
+        this.user.username = this.$store.getters.getUser.username
+        this.user.avatar = this.$store.getters.getUser.avatar
+        this.hasLogin = true
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+
+  .m-content {
+    max-width: 960px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .maction {
+    margin: 10px 0;
+  }
+
+</style>
